0af2c8cecca4e3d78a454cb4d452efcaec5058b9
Petyo <petyohristov2003@gmail.com>
2022-11-26 21:46:46 +0200

    Claims principal extension added

---
+++ Web/LegendsCoach.Web.Infrastructure/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace LegendsCoach.Web.Infrastructure.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Claims;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class ClaimsPrincipalExtension
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+    }
+}
