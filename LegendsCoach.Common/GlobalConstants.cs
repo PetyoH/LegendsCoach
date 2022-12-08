687fdbc004a4ef0ce4c32ade526f18df658acab8
Petyo <petyohristov2003@gmail.com>
2022-12-08 18:57:21 +0200

    Moderator role added

--- LegendsCoach.Common/GlobalConstants.cs
+++ LegendsCoach.Common/GlobalConstants.cs
@@ -5,5 +5,7 @@
         public const string SystemName = "LegendsCoach";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string ModeratorRoleName = "Moderator";
     }
 }
